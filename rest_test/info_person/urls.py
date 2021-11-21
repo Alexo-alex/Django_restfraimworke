42670c6988766d1454d3e03cd17ae46a8bcb8642
Alexo-alex <rubak161999@gmail.com>
2021-11-21 20:45:04 +0200

    init

--- rest_test/info_person/urls.py
+++ rest_test/info_person/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("person/", PersonListView.as_view()),
-    path("person_url/", get_persons),
     path("person/<int:pk>/", PersonDetailView.as_view()),
     path("company/", CompanyListView.as_view()),
     path("company/<int:pk>/", CompanyDetailView.as_view()),
